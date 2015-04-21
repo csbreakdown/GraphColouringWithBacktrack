a44391ff1d70b4f25d8975ff6bda2a7232a93091
krispyk84 <karimhamasni@gmail.com>
2015-04-21 06:09:56 -0400

    Publish

---
+++ src/mainProgram.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+/*
+ * Written by Karim Hamasni
+ * Graph Colouring using Backtracking
+ * April 20, 2015
+ * for CSBreakdown.com
+ */
+public class mainProgram {
+	public static void main(String[] args){
+		graphColour(0);
+	}
+	
+	static int[] x = {0,0,0,0};
+	static int m = 3;
+	static int n = 4;
+	static int[][] G = {{1,1,0,1},{1,1,1,1},{0,1,1,1},{1,1,1,1}};
+		
+	public static void graphColour(int k){
+		if(k == 4){
+			System.out.println(Arrays.toString(x));
+			return;
+		}
+		for(int c = 1; c<=m; c++){
+			if(isSafe(k,c)){
+				x[k] = c;
+				graphColour(k+1);
+			}
+		}
+	}
+	
+	public static boolean isSafe(int k, int c){
+		for(int i = 0; i< n; i++){
+			if(G[k][i] == 1 && c == x[i]){
+				return false;
+			}
+		}
+		return true;
+	}
+}
